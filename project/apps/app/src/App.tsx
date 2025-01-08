faf46395ae429ee03a5c1be8519c0c91f7e2615d
Hritik Gupta <v-hritgupta@microsoft.com>
2025-01-08 15:57:11 +0530

    task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -3,6 +3,17 @@ import { List } from 'ui'
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
 const App = () => {
+
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    fetch(api)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setPokemon(data)
+      });
+  }, [])
   return (
   <>
     <h1>Pokemon list:</h1>
